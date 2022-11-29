87027c88e944a5508a7c6d7a83c1c9a4482a991d
Ruben Asenjo Vega <rubenav82@gmail.com>
2022-11-29 16:37:58 +0100

    Se agrega el proyecto

---
+++ src/test/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author ruben.asenjo
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
